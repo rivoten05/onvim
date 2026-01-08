0733bc493f14424cdb3ec443f91b53889fe94645
rivo <rivo@gmail.com>
2026-01-08 17:37:49 +0530

    remove colorify

--- lua/plugins/colorify.lua
+++ lua/plugins/colorify.lua
@@ -1,10 +1,8 @@
 return {
   {
-    "nvchad-colorify", -- A unique name for your local plugin
-    dir = vim.fn.stdpath("config") .. "/lua/nvchad/colorify", -- Path to your files
-    lazy = false, -- Load on startup so it can set up autocommands
+    "rivoten05/colorify.nvim",
     config = function()
-      require("nvchad.colorify").run()
+      require("colorify").run()
     end,
   },
 }
